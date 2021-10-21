89cecf061cd078918af90677b9ee4d329d1955d0
liamjohnsexton <61158344+liamjohnsexton@users.noreply.github.com>
2021-10-21 12:34:36 +0100

    Update filter-instances.py

--- filter-instances.py
+++ filter-instances.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
 
-# File: filter-instances.py 
-# Description: This is an example script that you can author or modify that retrieves 
-#              a list of instances from the Relay Interface (in the form of parameters)
-#              and filters the instances that are untagged. It then sets the output
-#              variable `instanceIDs` to the list of instances that are untagged. 
-# Inputs:
-#   - instances - List of instances to evaluate 
-# Outputs:
-#   - instanceIDs - list of instance IDs to stop in the next step
-
 from relay_sdk import Interface, Dynamic as D
 
 relay = Interface()
